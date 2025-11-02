d9e1153d12741dc3d2f6c79b0fdd83b1eac95f95
tivily <tuncokkamil@gmail.com>
2025-11-03 00:37:19 +0300

    customerId departed from rental

--- Entities/Concrete/Location.cs
+++ Entities/Concrete/Location.cs
@@ -13,5 +13,7 @@ namespace Entities.Concrete
         public string Address { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
     }
 }
